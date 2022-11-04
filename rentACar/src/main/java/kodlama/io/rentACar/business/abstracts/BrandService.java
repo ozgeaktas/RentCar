c1bbe59130b3cfd9dbe598a757b0aee4f166eb54
ozgeaktas <54955167+ozgeaktas@users.noreply.github.com>
2022-11-04 14:05:19 +0300

    2. ders sonu

--- rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -2,9 +2,11 @@ package kodlama.io.rentACar.business.abstracts;
 
 import java.util.List;
 
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandResponse;
 
 public interface BrandService {
-	List<Brand> getAll();
+	List<GetAllBrandResponse> getAll();
+	void add(CreateBrandRequest createBrandRequest);
 
 }
--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandResponse;
+
 
 @RestController
 @RequestMapping("/api/brands")
@@ -22,9 +25,14 @@ public class BrandsController {
 	}
 	
 	@GetMapping("/getAll")
-	public List<Brand> getAll(){
+	public List<GetAllBrandResponse> getAll(){
 		return brandService.getAll();
 	}
+	@PostMapping("/add")
+		public void add(CreateBrandRequest createBrandRequest) {
+			this.brandService.add(createBrandRequest);
+	}
+	
 	
 
 }
